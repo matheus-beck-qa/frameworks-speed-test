880b1907e6b21aa67af0a887d6be6d9b822dcd17
matheus-beck_1701 <matheus.beck@2k.com>
2025-02-10 22:47:53 +0000

    generate chart

--- cypress/e2e/test/cypress_test.cy.js
+++ cypress/e2e/test/cypress_test.cy.js
@@ -1,18 +1,20 @@
 describe('SauceDemo Checkout Test', () => {
   it('Logs in, adds item to cart, and completes checkout', () => {
-    cy.visit('https://www.saucedemo.com/');
-    cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button').click();
-    cy.url().should('include', '/inventory.html');
-    cy.get('.inventory_item:first-child button').click();
-    cy.get('.shopping_cart_link').click();
-    cy.get('.checkout_button').click();
-    cy.get('#first-name').type('John');
-    cy.get('#last-name').type('Doe');
-    cy.get('#postal-code').type('12345');
-    cy.get('#continue').click();
-    cy.get('#finish').click();
-    cy.get('.complete-header').should('have.text', 'Thank you for your order!');
+    for (let i=0; i<100; i++){
+      cy.visit('https://www.saucedemo.com/');
+      cy.get('#user-name').type('standard_user');
+      cy.get('#password').type('secret_sauce');
+      cy.get('#login-button').click();
+      cy.url().should('include', '/inventory.html');
+      cy.get('.inventory_item:first-child button').click();
+      cy.get('.shopping_cart_link').click();
+      cy.get('.checkout_button').click();
+      cy.get('#first-name').type('John');
+      cy.get('#last-name').type('Doe');
+      cy.get('#postal-code').type('12345');
+      cy.get('#continue').click();
+      cy.get('#finish').click();
+      cy.get('.complete-header').should('have.text', 'Thank you for your order!');
+    }
   });
 });
--- playwright_test.js
+++ playwright_test.js
@@ -1,26 +1,28 @@
 import { chromium } from 'playwright';
 import assert from 'assert';
-(async () => {
-    const startTime = Date.now();
-    const browser = await chromium.launch({ headless: true, channel: 'chrome' });
-    const page = await browser.newPage();
-    await page.goto('https://www.saucedemo.com/');
-    await page.fill('#user-name', 'standard_user');
-    await page.fill('#password', 'secret_sauce');
-    await page.click('#login-button');
-    await page.waitForSelector('.inventory_list');
-    await page.click('.inventory_item:first-child button'); 
-    await page.click('.shopping_cart_link');
-    await page.click('.checkout_button');
-    await page.fill('#first-name', 'John');
-    await page.fill('#last-name', 'Doe');
-    await page.fill('#postal-code', '12345');
-    await page.click('#continue');
-    await page.click('#finish');
-    const successMessage = await page.innerText('.complete-header');
-    assert.strictEqual(successMessage, 'Thank you for your order!');
-    const endTime = Date.now();
-    const executionTime = (endTime - startTime) / 1000;
-    console.log(`Playwright Test Execution Time: ${executionTime} seconds`);
-    await browser.close();
-})();
+for (let i=0; i<100; i++){
+    (async () => {
+        //const startTime = Date.now();
+        const browser = await chromium.launch({ headless: true, channel: 'chrome' });
+        const page = await browser.newPage();
+        await page.goto('https://www.saucedemo.com/');
+        await page.fill('#user-name', 'standard_user');
+        await page.fill('#password', 'secret_sauce');
+        await page.click('#login-button');
+        await page.waitForSelector('.inventory_list');
+        await page.click('.inventory_item:first-child button'); 
+        await page.click('.shopping_cart_link');
+        await page.click('.checkout_button');
+        await page.fill('#first-name', 'John');
+        await page.fill('#last-name', 'Doe');
+        await page.fill('#postal-code', '12345');
+        await page.click('#continue');
+        await page.click('#finish');
+        const successMessage = await page.innerText('.complete-header');
+        assert.strictEqual(successMessage, 'Thank you for your order!');
+        // const endTime = Date.now();
+        // const executionTime = (endTime - startTime) / 1000;
+        // console.log(`Playwright Test Execution Time: ${executionTime} seconds`);
+        await browser.close();
+    })();
+}
